f068f4a729dd5b11cb7fe0080203c60192806f5a
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2025-01-30 17:32:01 +0900

    style: rounded 수정

--- src/app/chat/components/Questions/Question.tsx
+++ src/app/chat/components/Questions/Question.tsx
@@ -1,5 +1,5 @@
-import clsx from "clsx";
-import { QuestionType } from "../../data";
+import clsx from 'clsx';
+import { QuestionType } from '../../data';
 
 interface QuestionProps {
   id: number;
@@ -11,16 +11,11 @@ interface QuestionProps {
 
 const Question = ({ id, type, question, isActive, isPrev }: QuestionProps) => {
   return (
-    <div
-      key={id}
-      className={clsx("flex h-full items-center", isPrev && "justify-end")}
-    >
+    <div key={id} className={clsx('flex h-full items-center', isPrev && 'justify-end')}>
       <button
         className={clsx(
-          "flex h-full flex-col gap-[0.6rem] rounded-lg px-[1.6rem] py-[2rem] text-start transition-transform",
-          isActive
-            ? "h-full w-[27rem] bg-blue-100"
-            : "h-[11.4rem] w-[23rem] bg-gray-200",
+          'flex h-full flex-col gap-[0.6rem] rounded-[0.8rem] px-[1.6rem] py-[2rem] text-start transition-transform',
+          isActive ? 'h-full w-[27rem] bg-blue-100' : 'h-[11.4rem] w-[23rem] bg-gray-200'
         )}
       >
         <p className="text-body-3-semibold text-blue-500">{type}</p>
