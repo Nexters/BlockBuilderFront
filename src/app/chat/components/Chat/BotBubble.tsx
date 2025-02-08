3cca17bfd94d2a3732306c5da68dc81aeee0fe0c
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2025-02-08 15:43:58 +0900

    fix: 봇 채팅 줄바꿈 처리

--- src/app/chat/components/Chat/BotBubble.tsx
+++ src/app/chat/components/Chat/BotBubble.tsx
@@ -33,12 +33,12 @@ const BotBubble = memo(
     }, [handleFinishAnswering, text, words]);
 
     return (
-      <div className="flex gap-[1.6rem]">
+      <div className="flex w-full gap-[1.6rem]">
         <div className="size-[4rem] shrink-0">
           <Image src="/images/chat/bot-profile.png" alt="bot-profile" width={40} height={40} />
         </div>
         <div
-          className="whitespace-break-spaces text-body-1-medium"
+          className="max-w-[calc(100%-5.6rem)] flex-1 whitespace-break-spaces text-body-1-medium [&>*]:w-full [&>*]:whitespace-pre-line"
           dangerouslySetInnerHTML={{ __html: isLoading ? '답변 생성중입니다..' : displayText }}
         />
       </div>
