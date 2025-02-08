040d5fd418521eb2bf6e71a4ace03232aea08783
위영민 <youngminieo1005@gmail.com>
2025-02-09 02:59:01 +0900

    fix: type

--- src/app/information/type.ts
+++ src/app/information/type.ts
@@ -1,5 +1,5 @@
 type TBlockchainCategoryCode = '01' | '02' | '03' | '04';
-type TBlockchainNetworkCode = '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10';
+export type TBlockchainNetworkCode = '00' | '01' | '02' | '03' | '04' | '05' | '06' | '07' | '08' | '09' | '10';
 
 export type TBlockchainInformationApiData = {
   id: number;
