d3ebdfa1994d6dd278ac23c746a594d60f827c71
위영민 <youngminieo1005@gmail.com>
2025-02-12 00:55:43 +0900

    feat: 뉴스 섹션 Loading Lottie

--- src/app/information/components/BlockchainNewsSection/BlockchainNewsSection.client.tsx
+++ src/app/information/components/BlockchainNewsSection/BlockchainNewsSection.client.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import loadingAnimation from '@/assets/lotties/loading.json';
+import { useLottie } from 'lottie-react';
 import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { fetchBlockchainInformation } from '../../api/fetchBlockchainInformation';
@@ -18,13 +20,13 @@ const BlockchainNewsSectionClient = ({
   const { ref, inView } = useInView();
 
   const [newsList, setNewsList] = useState<TBlockchainInformationData[]>(initialData);
-  const [isLoading, setIsLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(initialCurrentPage + 1);
   const [hasNextPage, setHasNextPage] = useState(initialCurrentPage < initialTotalPage);
 
+  const { View } = useLottie({ animationData: loadingAnimation, loop: true }, { width: 100, height: 60 });
+
   useEffect(() => {
     const fetchNextPage = async () => {
-      setIsLoading(true);
       const response = await fetchBlockchainInformation({
         page: currentPage,
         size: 20,
@@ -34,15 +36,15 @@ const BlockchainNewsSectionClient = ({
       setNewsList((prevNewsList) => [...prevNewsList, ...response.data]);
       setCurrentPage(response.currentPage + 1);
       setHasNextPage(response.currentPage < response.totalPages);
-      setIsLoading(false);
     };
 
-    if (!inView || !hasNextPage || isLoading) {
+    if (!inView || !hasNextPage) {
       return;
     }
 
     fetchNextPage();
-  }, [inView, isLoading, currentPage, hasNextPage]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inView, hasNextPage]);
 
   // TODO: Empty, Loading Spinner UI
   return (
@@ -53,9 +55,9 @@ const BlockchainNewsSectionClient = ({
         })}
       </ul>
 
-      {!isLoading && hasNextPage && (
-        <div ref={ref} className="flex h-[10rem] items-center justify-center">
-          loading..
+      {hasNextPage && (
+        <div ref={ref} className="flex items-center justify-center pt-[4.6rem]">
+          {View}
         </div>
       )}
     </>
