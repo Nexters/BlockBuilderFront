a8c89490cc10d0d72f1d78c9719b8ed73e95f5f0
위영민 <youngminieo1005@gmail.com>
2025-02-20 22:37:41 +0900

    feat: Suspense 적용

--- src/app/information/page.tsx
+++ src/app/information/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import BlockchainEventSection from './components/BlockchainEventSection/BlockchainEventSection.server';
 import BlockchainNewsSection from './components/BlockchainNewsSection/BlockchainNewsSection.server';
 
@@ -10,15 +11,43 @@ export default function InformationPage() {
       >
         <div>
           <h2 className="px-[4rem] pb-[2rem] text-title-1-semibold text-gray-900 mobile:px-[2rem]">이벤트</h2>
-          <BlockchainEventSection />
+          <Suspense fallback={<BlockchainEventSectionSkeleton />}>
+            <BlockchainEventSection />
+          </Suspense>
         </div>
 
         <div className="px-[4rem] mobile:px-[2rem]">
           <h2 className="pb-[0.8rem] text-title-1-semibold text-gray-900">최신 뉴스</h2>
           <p className="text-body-2-regular text-gray-700">블록체인 관련 최신 뉴스를 확인하세요!</p>
-          <BlockchainNewsSection />
+          <Suspense fallback={<BlockchainNewsSectionSkeleton />}>
+            <BlockchainNewsSection />
+          </Suspense>
         </div>
       </div>
     </main>
   );
 }
+
+const BlockchainEventSectionSkeleton = () => {
+  return (
+    <div>
+      <div className="mb-[2.4rem] ml-[4rem] h-[3.2rem] w-[10rem] animate-pulse bg-gray-400 mobile:ml-[2rem]" />
+
+      <div className="grid grid-flow-col grid-rows-2 gap-[2rem] pl-[4rem] mobile:pl-[2rem]">
+        {Array.from({ length: 4 }).map((_, index) => (
+          <div key={`BlockchainEventSectionSkeleton_${index}`} className="h-[14.4rem] animate-pulse bg-gray-400" />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const BlockchainNewsSectionSkeleton = () => {
+  return (
+    <div className="grid grid-cols-1 gap-x-[1.5rem] gap-y-[2.4rem] pt-[2.4rem] tablet:grid-cols-2 desktop:grid-cols-3">
+      {Array.from({ length: 6 }).map((_, index) => (
+        <div key={`BlockchainEventSectionSkeleton_${index}`} className="h-[15rem] animate-pulse bg-gray-400" />
+      ))}
+    </div>
+  );
+};
