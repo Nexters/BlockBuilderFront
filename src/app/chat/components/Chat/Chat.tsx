8c4817d6a0b5a3d62099272a0a7d3faecee73c16
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2025-02-21 22:15:42 +0900

    fix: 채팅 너비 수정

--- src/app/chat/components/Chat/Chat.tsx
+++ src/app/chat/components/Chat/Chat.tsx
@@ -12,7 +12,7 @@ interface ChatProps {
 
 const Chat = ({ chatList, isLoading, handleFinishAnswering, isAnswering, recreateChat }: ChatProps) => {
   return (
-    <div className="flex h-[calc(100vh-13.8rem)] w-full flex-col gap-[4rem] overflow-y-auto px-[4rem] py-[6.4rem] mobile:h-[calc(100vh-19.4rem)]">
+    <div className="mx-auto flex h-[calc(100vh-13.8rem)] w-full max-w-[68.4rem] flex-col gap-[4rem] overflow-y-auto px-[4rem] py-[6.4rem] mobile:h-[calc(100vh-19.4rem)]">
       {chatList.map((chat) =>
         chat.isUser ? (
           <UserBubble key={chat.id} text={chat.text} />
